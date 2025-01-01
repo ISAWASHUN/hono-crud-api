6fc57b1010f87e7f7e3c611e2b391e5214adcaf1
ISAWASHUN <syun20000616@icloud.com>
2025-01-01 22:09:16 +0900

    GET Methodの作成

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,21 @@
+import { drizzle } from 'drizzle-orm/d1'
 import { Hono } from 'hono'
+import { todoTable } from '../db/schema'
 
-const app = new Hono()
+const app = new Hono<{Bindings: {DB: D1Database}}>()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+
+app.get('/health_cheack', (c) => {
+  c.status(200)
+  return c.body("Status OK!!")
 })
+
+// Todo API
+app.
+  get('/todos', async (c) => {
+    const db = drizzle(c.env.DB)
+    const getAllTodos = await db.select().from(todoTable)
+    return c.json(getAllTodos)
+  })
 
 export default app
